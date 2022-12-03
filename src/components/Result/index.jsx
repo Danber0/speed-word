ecdd660f99f16fb5066ecb5a90203588c0d8b13e
danber <bergauzen.danil@mail.ru>
2022-12-03 18:21:45 +0300

    fix timer

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -5,9 +5,9 @@ export const Result = ({ stats }) => {
     <div className="flex result">
       <div className="result__top-icon">😓</div>
       <p className="result__phrase">
-        Неплохо! За <b>{stats.sec} секунд</b>, ты ввел:
+        Неплохо! За <b>20 секунд</b>, ты ввел:
       </p>
-      <div className="result__number-of-words">{stats.words} слова</div>
+      <div className="result__number-of-words">{stats.words} слов</div>
       <button className="button" onClick={() => window.location.reload()}>
         🤔 Попробовать снова?
       </button>
