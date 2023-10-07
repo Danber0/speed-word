45103bbb7409c9b1758b9feafea68b21656403e3
Danber <steem.sobol@mail.ru>
2023-10-06 22:43:36 -0400

    Fixed

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 export const Result = ({ stats }) => {
+  console.log(stats.sec);
   return (
     <div className="flex result">
       <div className="result__top-icon">😓</div>
       <p className="result__phrase">
-        Not bad! <b>{20 - stats.sec} seconds</b>, your input:
+        Not bad! <b>{20 - stats.sec === 0 ? 20 : 20 - stats.sec} seconds</b>,
+        your input:
       </p>
       <div className="result__number-of-words">{stats.words} words</div>
       <button className="button" onClick={() => window.location.reload()}>
